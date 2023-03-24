d37e99bbe11a7f04e988c22b3dd4dc883e0b0661
Jerome Leclanche <jerome@leclan.ch>
2023-03-24 03:25:08 +0100

    Refactor

--- stripe_receipts/management/commands/load_stripe_receipt.py
+++ stripe_receipts/management/commands/load_stripe_receipt.py
@@ -3,8 +3,8 @@ from io import BytesIO
 import requests
 from django.core.management.base import BaseCommand
 
-from financica.apps.stripe_receipts.models import Receipt
-from financica.utils import get_content_disposition_filename
+from ...models import Receipt
+from ...utils import parse_html_receipt, urls_for_id
 
 
 class Command(BaseCommand):
@@ -19,7 +19,7 @@ class Command(BaseCommand):
                 self.stdout.write("Receipt {receipt_id} already exists, skipping…")
                 continue
 
-            receipt_url, receipt_pdf_url = Receipt.urls_for_id(receipt_id)
+            receipt_url, receipt_pdf_url = urls_for_id(receipt_id)
 
             resp = requests.get(receipt_url)
             try:
@@ -33,36 +33,38 @@ class Command(BaseCommand):
                     id=receipt_id,
                     defaults={
                         "type": receipt_id.rpartition("_")[0],
-                        **Receipt.parse_html_receipt(resp.content),
+                        **parse_html_receipt(resp.content),
                     },
                 )
 
                 # Save the HTML receipt
-                receipt.set_receipt_html(
-                    BytesIO(resp.content),
-                    filename=f"{receipt_id}.html",
-                    source_url=resp.request.url,
-                    metadata={"type": "RECEIPT_EMAIL"},
-                )
+                # receipt.set_receipt_html(
+                #     BytesIO(resp.content),
+                #     filename=f"{receipt_id}.html",
+                #     source_url=resp.request.url,
+                #     metadata={"type": "RECEIPT_EMAIL"},
+                # )
 
                 # Download receipt as PDF
                 # (Expect a redirect, so allow_redirects=False)
                 resp = requests.get(receipt_pdf_url, allow_redirects=False)
                 if resp.status_code == 302:
                     redir_url = resp.headers.get("location")
+                    if not redir_url:
+                        raise ValueError("No location header in redirect response")
                     resp = requests.get(redir_url, stream=True)
-                    receipt.set_receipt_pdf(
-                        resp.raw,
-                        filename=get_content_disposition_filename(
-                            resp.headers["content-disposition"]
-                        ),
-                        source_url=redir_url,
-                        metadata={
-                            "type": "RECEIPT",
-                            "stripe_original_url": receipt_pdf_url,
-                            "stripe_file_id": Receipt.parse_file_id(redir_url),
-                        },
-                    )
+                    # receipt.set_receipt_pdf(
+                    #     resp.raw,
+                    #     filename=get_content_disposition_filename(
+                    #         resp.headers["content-disposition"]
+                    #     ),
+                    #     source_url=redir_url,
+                    #     metadata={
+                    #         "type": "RECEIPT",
+                    #         "stripe_original_url": receipt_pdf_url,
+                    #         "stripe_file_id": parse_file_id(redir_url),
+                    #     },
+                    # )
                 else:
                     self.stderr.write(
                         f"PDF not available (status code {resp.status_code})"
--- stripe_receipts/models.py
+++ stripe_receipts/models.py
@@ -1,16 +1,6 @@
-import re
-
-import bs4
 from django.db import models
 
-from financica.lib.database.fields import FileAttachmentField
-
-STRIPE_PAY = "https://pay.stripe.com"
-STRIPE_DASH = "https://dashboard.stripe.com"
-
-
-def _first_string_of_items(items, default=""):
-    return (list(set(items)) + [""])[0]
+from .utils import STRIPE_DASH, STRIPE_PAY
 
 
 class Receipt(models.Model):
@@ -61,68 +51,19 @@ class Receipt(models.Model):
         help_text="End-user visible invoice number",
     )
 
-    receipt_html = FileAttachmentField(
-        upload_to="stripe/receipts/",
-        related_name="stripe_html_receipts",
-        help_text="The receipt, as HTML",
-    )
-    receipt_pdf = FileAttachmentField(
-        upload_to="stripe/receipts/",
-        related_name="stripe_pdf_receipts",
-        help_text="The receipt, as PDF (sometimes unavailable)",
-    )
+    # receipt_html = FileAttachmentField(
+    #     upload_to="stripe/receipts/",
+    #     related_name="stripe_html_receipts",
+    #     help_text="The receipt, as HTML",
+    # )
+    # receipt_pdf = FileAttachmentField(
+    #     upload_to="stripe/receipts/",
+    #     related_name="stripe_pdf_receipts",
+    #     help_text="The receipt, as PDF (sometimes unavailable)",
+    # )
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
-    @staticmethod
-    def urls_for_id(receipt_id: str):
-        url = f"{STRIPE_DASH}/emails/receipts/{receipt_id}"
-        return url, url + "/pdf"
-
-    # TODO: move to financica.lib package
-    @staticmethod
-    def parse_html_receipt(data: bytes):
-        soup = bs4.BeautifulSoup(data.decode(), "html.parser")
-        html = soup.find("html")
-        language = html.attrs["lang"]
-        if language not in ("en", "fr"):
-            raise NotImplementedError(
-                f"Stripe Receipt parsing for language {language!r} not implemented"
-            )
-        _ = {
-            "INVOICE_NUM": {
-                "en": r"Invoice #([A-Z\d-]+)",
-                "fr": r"Facture n°([A-Z\d-]+)",
-            },
-            "RECEIPT_NUM": {"en": r"#([\d-]+)", "fr": r"n°([\d-]+)"},
-        }
-        title = html.find("title").text
-        testmode = title.startswith("TEST - ")
-        table_text = " ".join(x.text for x in soup.find_all("table"))
-        # add an empty string to default to an empty string if we haven't found any invoice number
-        invoice_number = _first_string_of_items(
-            re.findall(_["INVOICE_NUM"][language], table_text)
-        )
-        account_id = _first_string_of_items(
-            re.findall(r"/(acct_[A-Za-z\d]+)/", str(soup))
-        )
-
-        return {
-            "language": language,
-            "livemode": not testmode,
-            "receipt_number": re.findall(_["RECEIPT_NUM"][language], title)[-1],
-            "account_id": account_id,
-            "invoice_number": invoice_number,
-        }
-
-    # TODO: move to financica.lib package
-    @staticmethod
-    def parse_file_id(url: str) -> str:
-        sre = re.match(r"https://files.stripe.com/files/(f_[A-Za-z0-9_]+)", url)
-        if sre:
-            return sre.group(1)
-        return ""
 
     @property
     def permalink(self):
